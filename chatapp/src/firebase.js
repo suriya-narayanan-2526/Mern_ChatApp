c530b9c7940c16c2c60832226b32d6ca901ec490
Suriya Narayanan <suriyanarayanan@gmail.com>
2026-01-14 14:19:16 +0530

    updated after front end deployment

---
+++ chatapp/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwcX9_KoSGVXxonLMhxoT1xJTe_iykxuw",
+  authDomain: "suriyalocalchat-hub.firebaseapp.com",
+  projectId: "suriyalocalchat-hub",
+  storageBucket: "suriyalocalchat-hub.firebasestorage.app",
+  messagingSenderId: "30991444976",
+  appId: "1:30991444976:web:324716b1453145991ecb35"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
